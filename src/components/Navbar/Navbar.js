3ee9c8f4ce8626233d7756e624063582106d1620
ayadi1 <ayadiossama44@gmail.com>
2022-12-08 12:13:41 +0100

    update Navbar links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,24 +2,21 @@ import "./Navbar.css";
 import { logo } from "../../assets";
 import { HiOutlineMenuAlt1, HiX } from "react-icons/hi";
 import { useState } from "react";
-const Links = () => (
-  <>
-    <a href="#link">Home</a>
-    <a href="#link">About Us</a>
-    <a href="#link">Features</a>
-    <a href="#link">Solution</a>
-  </>
-);
+import { navLinks } from "../../constants";
+const Links = navLinks.map((link) => (
+  <a href={"#" + link.title} key={link.id}>
+    {link.title}
+  </a>
+));
 export default function Navbar() {
+  console.log(Links);
   const [isShowing, setIsShowing] = useState(false);
   return (
     <div className="app__Navbar">
       <div className="app__Navbar-logo">
         <img src={logo} alt="logo" />
       </div>
-      <div className="app__Navbar-links">
-        <Links />
-      </div>
+      <div className="app__Navbar-links">{Links}</div>
       <div className="app__Navbar-icons">
         {isShowing ? (
           <HiX color="#fff" onClick={() => setIsShowing((old) => !old)} />
@@ -32,9 +29,7 @@ export default function Navbar() {
       </div>
       <div className="app__Navbar-mobile-menu ">
         {isShowing && (
-          <div className="app__Navbar-mobile-menu-links sidebar">
-            <Links />
-          </div>
+          <div className="app__Navbar-mobile-menu-links sidebar">{Links}</div>
         )}
       </div>
     </div>
