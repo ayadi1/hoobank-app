d30d931e1bb42be20d18f4c9b6c6a6d7daa41014
ayadi1 <ayadiossama44@gmail.com>
2022-12-08 14:30:27 +0100

    feat: add Billing section

--- src/components/Billing/Billing.js
+++ src/components/Billing/Billing.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import React from "react";
 import "./Billing.css";
-
+import { apple, google , bill } from "../../assets";
 export default function Billing() {
   return (
-    <div>Billing</div>
-  )
+    <div className="app__Billing">
+      <div className="app__Billing-image">
+        <img src={bill} alt="" />
+      </div>
+      <div className="app__Billing-content">
+        <h2>Easily control your billing & invoicing.</h2>
+        <p>
+          Elit enim sed massa etiam. Mauris eu adipiscing ultrices ametodio
+          aenean neque. Fusce ipsum orci rhoncus aliporttitor integer platea
+          placerat.
+        </p>
+        <div className="app__Billing-content-download">
+          <img src={apple} alt="apple" />
+          <img src={google} alt="google" />
+        </div>
+      </div>
+    </div>
+  );
 }
