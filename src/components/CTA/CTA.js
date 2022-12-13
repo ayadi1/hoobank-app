4542548651d31c8ba500e94bf6aebf2d611cb459
ayadi1 <ayadiossama44@gmail.com>
2022-12-13 11:03:12 +0100

    add CTA section

--- src/components/CTA/CTA.js
+++ src/components/CTA/CTA.js
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from "react";
 import "./CTA.css";
 
 export default function CTA() {
   return (
-    <div>CTA</div>
-  )
+    <div className="app__CTA">
+      <div className="app__CTA-headlines">
+        <h2>Let’s try our service now!</h2>
+        <p>
+          Everything you need to accept card payments and grow your business
+          anywhere on the planet.
+        </p>
+      </div>
+      <div className="app__CTA-buttons">
+        <button>Get Started</button>
+      </div>
+    </div>
+  );
 }
